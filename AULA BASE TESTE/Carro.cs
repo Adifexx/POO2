04b8ecdf089993fc5f74e1a070894dcad4a66d5c
Adifexx <arthuradnet@hotmail.com>
2026-08-07 10:20:45 -0300

    Add files via upload

---
+++ AULA BASE TESTE/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TESTEE
+{
+    internal class Carro
+    {
+        public string? Modelo { get; set; }
+        public int Ano {  get; set; }
+        public float Velocidade { get; set; }
+
+
+        public void Acelerar()
+        {
+            Velocidade++;
+            Console.WriteLine("Velocidade do carro e ", Velocidade);
+        }
+
+
+        public void Frear() => Console.WriteLine("Freando Carro...");
+
+        //public void Frear()
+        //{
+        //    Console.WriteLine("Freando Carro...");
+        //}
+    }
+}
---
+++ AULA BASE TESTE/Program.cs
@@ -0,0 +1,16 @@
+﻿using TESTEE;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+            c1.Modelo = "Fusca";
+            c1.Ano = 1990;
+            c1.Velocidade = 1;
+        }
+    }
+}
+
