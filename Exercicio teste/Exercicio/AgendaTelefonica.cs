c501d037cd3370cfbb6ad3d9e8287db3d20601b2
Adifexx <arthuradnet@hotmail.com>
2026-08-07 10:51:44 -0300

    Add files via upload

---
+++ Exercicio teste/Exercicio/AgendaTelefonica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio
+{
+    internal class AgendaTelefonica
+    {
+        Dictionary<string, string> colecao {  get; set; }
+
+
+        public void inserir(string nome, string numero)
+        {
+            colecao.Add(nome, numero);
+        }
+
+        public string buscarNumero(string nome)
+        {
+            if (colecao.TryGetValue(nome, out string numero))
+            {
+                return numero;
+            }
+            else return "a";
+            }
+        
+
+        public void remover(string nome)
+        {
+            colecao.Remove(nome);
+        }
+
+        //public int tamanho()
+        //{
+
+        //}
+        
+    }
+}
---
+++ Exercicio teste/Exercicio/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercicio;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AgendaTelefonica ag = new AgendaTelefonica();
+        ag.inserir("Arthur1", "1");
+        ag.inserir("Arthur2", "2");
+        ag.inserir("Arthur3", "3");
+        ag.inserir("Arthur4", "4");
+
+        ag.buscarNumero("Arthur1");
+
+        Console.WriteLine($"O numero desse nome eh: {ag.buscarNumero("Arthur1")}");
+    }
+}
