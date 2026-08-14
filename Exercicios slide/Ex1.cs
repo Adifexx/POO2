327dd0d8ffe4a39acefc22b74c2bf859cf2bae40
Adifexx <arthuradnet@hotmail.com>
2026-08-14 10:47:00 -0300

    Add files via upload

---
+++ Exercicios slide/Ex1.cs
@@ -0,0 +1,63 @@
+﻿
+
+Console.WriteLine("Gestão de pessoas:");
+
+int idade;
+float total = 0, qtaA = 0, qtaB = 0, qtaC = 0, qtaD = 0, qtaE = 0;
+
+
+ Console.WriteLine("Digite a idade: ");
+
+while (true)
+{
+
+    idade = int.Parse(Console.ReadLine());
+
+    if (idade < 0)
+    {
+        Console.WriteLine("Idade inválida!");
+        continue;
+    }
+
+    if (idade == 0)
+    {
+        break;
+    }
+
+    switch (idade)
+    {
+        case < 16:
+            qtaA++;
+            total++;
+            break;
+        case >= 16 and < 31:
+            qtaB++;
+            total++;
+            break;
+        case >= 31 and < 46:
+            qtaC++;
+            total++;
+            break;
+        case >= 46 and < 61:
+            qtaD++;
+            total++;
+            break;
+        case >= 61:
+            qtaE++;
+            total++;
+            break;
+
+    }
+}
+
+Console.WriteLine($"A: {(qtaA / total) * 100:f2}%");
+Console.WriteLine($"B: {(qtaB / total) * 100:f2}%");
+Console.WriteLine($"C: {(qtaC / total) * 100:f2}%");
+Console.WriteLine($"D: {(qtaD / total) * 100:f2}%");
+Console.WriteLine($"E: {(qtaE / total) * 100:f2}%");
+
+
+
+
+
+
---
+++ Exercicios slide/Ex3.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Digite a temperatura: ");
+
+int dias = 0;
+float temp, tempTotal = 0;
+
+while (dias < 16)
+{
+    temp = float.Parse(Console.ReadLine());
+
+    if (temp < 28)
+    {
+        Console.WriteLine("temperatura invalida");
+        continue;
+    }
+
+    dias++;
+    tempTotal += temp;
+
+}
+
+Console.WriteLine($"Temperatura Media: {tempTotal/dias}");
